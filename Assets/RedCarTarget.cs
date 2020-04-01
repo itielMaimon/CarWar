902e16c0fda70c1b2422298e4576fa37a2498513
Itiel Maimon <itiel.maimon@gmail.com>
2020-04-01 16:59:35 +0300

    Added health bar.

--- Assets/RedCarTarget.cs
+++ Assets/RedCarTarget.cs
@@ -2,11 +2,20 @@
 
 public class RedCarTarget : MonoBehaviour
 {
-    public float health = 50f;
+    public float health = 100f;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        healthBar.SetMaxHealth(health);
+    }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
+
+        healthBar.SetHealth(health);
 
         Debug.Log("Hit");
 
