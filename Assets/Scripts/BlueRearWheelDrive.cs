7dd07c00406e32378218c6a7a713a737d39e7721
Itiel Maimon <itiel.maimon@gmail.com>
2020-04-05 22:12:44 +0300

    Removed unused resources

---
+++ Assets/Scripts/BlueRearWheelDrive.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class BlueRearWheelDrive : MonoBehaviour {
+
+	private WheelCollider[] wheels;
+
+	public float maxAngle = 30;
+	public float maxTorque = 300;
+	public GameObject wheelShape;
+
+	// here we find all the WheelColliders down in the hierarchy
+	public void Start()
+	{
+		wheels = GetComponentsInChildren<WheelCollider>();
+
+		for (int i = 0; i < wheels.Length; ++i) 
+		{
+			var wheel = wheels [i];
+
+			// create wheel shapes only when needed
+			if (wheelShape != null)
+			{
+				var ws = GameObject.Instantiate (wheelShape);
+				ws.transform.parent = wheel.transform;
+
+				if(wheel.transform.localPosition.x < 0f)
+				{
+					ws.transform.localScale = new Vector3(ws.transform.localScale.x * -1f, ws.transform.localScale.y, ws.transform.localScale.z);
+				}
+			}
+		}
+	}
+
+	// this is a really simple approach to updating wheels
+	// here we simulate a rear wheel drive car and assume that the car is perfectly symmetric at local zero
+	// this helps us to figure our which wheels are front ones and which are rear
+	public void Update()
+	{
+		float angle = maxAngle * Input.GetAxis("Horizontal1");
+		float torque = maxTorque * Input.GetAxis("Vertical1");
+
+		foreach (WheelCollider wheel in wheels)
+		{
+			// a simple car where front wheels steer while rear ones drive
+			if (wheel.transform.localPosition.z > 0)
+				wheel.steerAngle = angle;
+
+			if (wheel.transform.localPosition.z < 0)
+				wheel.motorTorque = torque;
+
+			// update visual wheels if any
+			if (wheelShape) 
+			{
+				Quaternion q;
+				Vector3 p;
+				wheel.GetWorldPose (out p, out q);
+
+				// assume that the only child of the wheelcollider is the wheel shape
+				Transform shapeTransform = wheel.transform.GetChild (0);
+				shapeTransform.position = p;
+				shapeTransform.rotation = q;
+			}
+
+		}
+	}
+}
---
+++ Assets/Scripts/RedGunAction.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class RedGunAction : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 50f;
+
+    public GameObject car;
+    public ParticleSystem flash;
+    public GameObject impactEffect;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        flash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(car.transform.position, car.transform.forward, out hit, range))
+        {
+            CarTarget target = hit.transform.GetComponent<CarTarget>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
